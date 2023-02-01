a45a3a0b65ac9e3e931d33c0be48a439b752c376
Christian Rodrigo <chri65mb@stud.kea.dk>
2023-02-01 21:38:20 +0100

    produkt side lavet udgave 1

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function nav() {
     /*  body.classList.remove("noScroll"); */
   });
 }
+
+let folder = document.getElementsByClassName("folder_menu");
+let i;
+
+for (i = 0; i < folder.length; i++) {
+  folder[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let info = this.nextElementSibling;
+    if (info.style.display === "block") {
+      info.style.display = "none";
+    } else {
+      info.style.display = "block";
+    }
+    if (info.style.maxHeight) {
+      info.style.maxHeight = null;
+    } else {
+      info.style.maxHeight = info.scrollHeight + "px";
+    }
+  });
+}
